cb6b1ef2f85349220782e9d2d8d268cbb49a538e
Ash Johns <ashleypjohns@gmail.com>
2018-11-20 23:48:32 -0500

    continue with tutorial through episode 20

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+import logo from './logo.svg';
+import MoviesList from './MoviesList';
+import MoviesDetail from './MovieDetail';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MoviesDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
